a215f5c22e1fd0d0f05695cae5c8b19b51c226a2
Willy Biti <61201244+Willy-biti@users.noreply.github.com>
2021-10-19 09:49:53 -0300

    Controlando comportamentos js
    Mexendo na js, controlando os itens da tela para atribuir funções neles

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = 
+[
+    {
+        titulo: 'vereador',
+        numero: 5,
+        candidato: [
+            {
+                numero : 38111,
+                nome: 'fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    {URL: '38111.jpg', legenda: 'vereador', small: false}
+                ]
+            },
+            {
+                numero : 77222,
+                nome: 'Baltrano da SIlva',
+                partido: 'DEFG',
+                fotos: [
+                    {URL: '77222.jpg', legenda: 'vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidato:[
+            {
+                numero : 99,
+                nome: 'CICLANO',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {URL: '99.jpg', legenda: 'PREFEITO'},
+                    {URL: '99_2.jpg', legenda: 'VICE-PREFEITO', small:true}
+                ]
+            },
+            {
+                numero : 84,
+                nome: 'ZULANO',
+                partido: 'QUERTY',
+                vice: 'Azul',
+                fotos: [
+                    {URL: '84.jpg', legenda: 'PREFEITO'},
+                    {URL: '84_2.jpg', legenda: 'VICE-PREFEITO', small:true}
+                ]
+            }
+        ]
+    }
+
+];
--- urna.js
+++ urna.js
@@ -1,1 +1,47 @@
-let test = 1;
+let seuVotoPar = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-direita');
+let numeros = document.querySelector('.d-1-3');
+
+
+let etapaAtual = 0;
+
+function comecarEtapa(){
+
+    let etapa = etapas[etapaAtual];
+
+    let numeroHTML = '';
+ 
+    for(let i=0;i<etapa.numeros; i++){
+        numeroHTML += '<div class="numero"></div>';
+    }
+
+    seuVotoPar.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = ' ';
+    aviso.style.display = 'none';
+    lateral.innerHTML = ' ';
+    numeros.innerHTML = numeroHTML;
+}
+
+
+function clicou(n){
+    alert('clicou em' + n);
+}
+
+function branco(){
+    alert('clicou em BRANCO' );
+}
+
+function corrige(){
+    alert('clicou em CORRIGE');
+}
+
+function confirma(){
+    alert('clicou em CONFIRMA');
+}
+
+
+comecarEtapa();
